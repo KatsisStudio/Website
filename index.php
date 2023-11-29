1f2d7beb5d23f47b82bc7f811c68addcd5946639
Xwilarg <xwilarg@protonmail.com>
2023-11-29 22:15:29 +0100

    Add Twig

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+$loader = new FilesystemLoader(["templates"]);
+$twig = new Environment($loader);
+
+echo $twig->render("index.html.twig", [
+    "members" => json_decode(file_get_contents("data/members.json"), true)
+]);
