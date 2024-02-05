1273c2f6fb3321dd23155666be7c119666757857
Xwilarg <xwilarg@protonmail.com>
2024-02-05 16:02:52 +0100

    Add hover effect

--- js/project.js
+++ js/project.js
@@ -1,4 +1,20 @@
 window.addEventListener("load", _ => {
+    let intervalFct;
+
+    let displayImg;
+    let displayPreviews;
+    let displayIndex;
+
+    function displayImages() {
+        if (displayIndex == displayPreviews.length) {
+            displayIndex = 0;
+        }
+
+        displayImg.src = displayPreviews[displayIndex];
+
+        displayIndex++;
+    }
+
     for (let project of document.getElementsByClassName("project-info")) {
         const previews = project.dataset.previews.split(';').filter(x => x !== "");
 
@@ -7,9 +23,17 @@ window.addEventListener("load", _ => {
 
             project.addEventListener("mouseover", _ => {
                 img.src = previews[0];
+
+                displayImg = img;
+                displayPreviews = previews;
+                displayIndex = 1;
+
+                intervalFct = setInterval(displayImages, 1000);
             });
             project.addEventListener("mouseout", _ => {
                 img.src = project.dataset.src;
+
+                clearInterval(intervalFct);
             });
         }
     }
