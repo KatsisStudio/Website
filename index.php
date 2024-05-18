ecf008e0be68ed507d9d37868c3899c235986bb0
Xwilarg <xwilarg@protonmail.com>
2024-05-19 01:30:37 +0200

    Add API

--- index.php
+++ index.php
@@ -8,8 +8,17 @@ use Twig\Environment;
 $loader = new FilesystemLoader(["templates"]);
 $twig = new Environment($loader);
 
-echo $twig->render("index.html.twig", [
-    "projects" => json_decode(file_get_contents("data/projects.json"), true),
-    "members" => json_decode(file_get_contents("data/members.json"), true),
-    "characters" => json_decode(file_get_contents("data/characters.json"), true)
-]);
+$json = isset($_GET["json"]) && $_GET["json"] === "1";
+if ($json) {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode([
+        "projects" => json_decode(file_get_contents("data/projects.json"), true),
+        "members" => json_decode(file_get_contents("data/members.json"), true)
+    ]);
+} else {
+    echo $twig->render("index.html.twig", [
+        "projects" => json_decode(file_get_contents("data/projects.json"), true),
+        "members" => json_decode(file_get_contents("data/members.json"), true),
+        "characters" => json_decode(file_get_contents("data/characters.json"), true)
+    ]);
+}
