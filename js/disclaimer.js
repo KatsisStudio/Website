9f616e5de4c7c57e18bb7f80fbe53beb15be177d
Xwilarg <xwilarg@protonmail.com>
2024-05-20 19:22:58 +0200

    Improve website responsiveness

--- js/disclaimer.js
+++ js/disclaimer.js
@@ -1,16 +1,34 @@
-window.addEventListener("load", _ => {
-    if (sessionStorage.getItem("consent") === "true") {
-        document.getElementById("disclaimer").hidden = true;
-        document.getElementById("content").hidden = false;
+// https://stackoverflow.com/a/24103596
+function setCookie(name, value)
+{
+    document.cookie = `${name}=${value}; expires=${new Date(new Date().setFullYear(new Date().getFullYear() + 1))}`;
+}
+function getCookie(name)
+{
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
+    return null;
+}
 
-    document.getElementById("entry-confirm").addEventListener("click", () => {
-        sessionStorage.setItem("consent", "true");
-        document.getElementById("disclaimer").hidden = true;
-        document.getElementById("content").hidden = false;
-    });
-
-    document.getElementById("entry-deny").addEventListener("click", () => {
-        window.location.href = "https://google.com";
-    });
-});
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        if (getCookie("consent") === "true") {
+            document.getElementById("disclaimer").hidden = true;
+            document.getElementById("content").hidden = false;
+        }
+        document.getElementById("entry-confirm").addEventListener("click", () => {
+            setCookie("consent", "true");
+            document.getElementById("disclaimer").hidden = true;
+            document.getElementById("content").hidden = false;
+        });
+    
+        document.getElementById("entry-deny").addEventListener("click", () => {
+            window.location.href = "https://google.com";
+        });
+    }
+};
