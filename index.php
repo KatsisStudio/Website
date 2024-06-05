9eca79007e22b8e58a2f768642516811f1165a26
Xwilarg <xwilarg@protonmail.com>
2024-06-05 19:41:11 +0200

    Remove submodule

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ if ($json) {
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode([
         "projects" => json_decode(file_get_contents("data/projects.json"), true),
-        "members" => json_decode(file_get_contents("data/members/members.json"), true)
+        "members" => json_decode(file_get_contents("data/members.json"), true)
     ]);
 } else {
     echo $twig->render("index.html.twig", [
         "projects" => json_decode(file_get_contents("data/projects.json"), true),
-        "members" => json_decode(file_get_contents("data/members/members.json"), true),
+        "members" => json_decode(file_get_contents("data/members.json"), true),
         "characters" => json_decode(file_get_contents("data/characters.json"), true)
     ]);
 }
