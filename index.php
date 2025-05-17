b69a74abedabf53d0b6102a6e85460763b802801
Xwilarg <xwilarg@protonmail.com>
2025-05-17 18:52:40 +0200

    Add new project

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ $json = isset($_GET["json"]) && $_GET["json"] === "1";
 if ($json) {
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode([
-        "projects" => json_decode(file_get_contents("data/projects.json"), true),
+        "projects" => array_reverse(json_decode(file_get_contents("data/projects.json"), true)),
         "members" => json_decode(file_get_contents("data/members.json"), true)
     ]);
 } else {
     echo $twig->render("index.html.twig", [
-        "projects" => json_decode(file_get_contents("data/projects.json"), true),
+        "projects" => array_reverse(json_decode(file_get_contents("data/projects.json"), true)),
         "members" => json_decode(file_get_contents("data/members.json"), true)
     ]);
 }
